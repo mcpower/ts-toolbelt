4cddcc23ccd45341bbb52f03ba0591061dca3e59
Michael <mcpower@users.noreply.github.com>
2019-09-30 21:44:26 +1000

    feat(p.pick): support unlimited nested arrays
    Requires TypeScript 3.7.

--- src/Object/P/Pick.ts
+++ src/Object/P/Pick.ts
@@ -9,18 +9,22 @@ import {EndOf} from '../../Tuple/EndOf'
 import {Tuple} from '../../Tuple/Tuple'
 
 type _Pick<O extends object, Path extends Tuple<Index>, I extends Iteration = IterationOf<'0'>> =
-  OPick<O, Path[Pos<I>]> extends infer Picked // Pick the first Path
-  ? {
-      [K in keyof Picked]:
-        Picked[K] extends infer Prop          // Needed for the below to be distributive
-        ? Prop extends object                 // > If it's an object
-          ? Pos<I> extends EndOf<Path>        // & If it's the target
-            ? Prop                            // 1-1: Pick it
-            : _Pick<Prop, Path, Next<I>>      // 1-0: Continue diving
-          : Prop                              // 0: Pick property
-        : never
-    }
-  : never
+  O extends (infer A)[]                         // If the object is an array
+  ? A extends object                            //   If the array is of objects
+    ? _Pick<A, Path, I>[]                       //     Pick from those objects
+    : O                                         //   Else, pick array for unions
+  : OPick<O, Path[Pos<I>]> extends infer Picked // Else, pick the first Path
+    ? {
+        [K in keyof Picked]:
+          Picked[K] extends infer Prop          // Needed for the below to be distributive
+          ? Prop extends object                 // > If it's an object
+            ? Pos<I> extends EndOf<Path>        // & If it's the target
+              ? Prop                            // 1-1: Pick it
+              : _Pick<Prop, Path, Next<I>>      // 1-0: Continue diving
+            : Prop                              // 0: Pick property
+          : never
+      }
+    : never
 
 /** Extract out of **`O`** the fields at **`Path`**
  * (⚠️ this type is expensive)
